4429b28670d3cae9c76e8423c97279d1ef1e8148
Vinesh Ghela <vineshghela@gmail.com>
2021-07-23 13:12:22 +0100

    more changes and cleaning up

--- app/src/main/java/com/qa/myapplication/MainActivity.kt
+++ app/src/main/java/com/qa/myapplication/MainActivity.kt
@@ -1,20 +1,17 @@
 package com.qa.myapplication
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-
 import android.widget.ArrayAdapter
 import android.widget.Button
+import android.widget.ListView
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import android.widget.ListView
-
 
 
 class MainActivity : AppCompatActivity() {
@@ -23,9 +20,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val button =findViewById<Button>(R.id.addPost)
+        val button = findViewById<Button>(R.id.addPost)
         button.setOnClickListener {
-            val intent = Intent(this@MainActivity,MainActivity2::class.java)
+            val intent = Intent(this@MainActivity, MainActivity2::class.java)
             startActivity(intent)
         }
 
@@ -38,24 +35,25 @@ class MainActivity : AppCompatActivity() {
         val api = retrofit.create(todoApi::class.java)
 
         api.getTodos()?.enqueue(object : Callback<List<Todos?>?> {
-            override fun onResponse(call: Call<List<Todos?>?>?,
-                                    response: Response<List<Todos?>?>) {
+            override fun onResponse(
+                call: Call<List<Todos?>?>?,
+                response: Response<List<Todos?>?>
+            ) {
                 val heroList: List<Todos?>? = response.body() // Now we can use
                 //this heroList for getting the data from API.
 
                 //Creating an String array for the ListView
                 val heroes = arrayOfNulls<String>(heroList?.size!!)
 
-
                 //looping through all the heroes and inserting the names
                 //  inside the string array
                 for (i in heroList.indices) {
-                    heroes[i] =heroList[i]?.id.toString() +" : "+ heroList[i]?.title +", "+ heroList[i]?.completed
-//                    Log.i("heroossss",""+heroes[i])
+                    heroes[i] =
+                        heroList[i]?.id.toString() + " : " + heroList[i]?.title + ", " + heroList[i]?.completed
                 }
 
                 //displaying the string array into listview
-                val listview =findViewById<ListView>(R.id.listView)
+                val listview = findViewById<ListView>(R.id.listView)
                 listview.adapter = ArrayAdapter(
                     applicationContext,
                     android.R.layout.simple_list_item_1,
@@ -64,12 +62,12 @@ class MainActivity : AppCompatActivity() {
             }
 
             override fun onFailure(call: Call<List<Todos?>?>?, t: Throwable) {
-                Toast.makeText(applicationContext, t.message,
-                    Toast.LENGTH_SHORT).show()
+                Toast.makeText(
+                    applicationContext, t.message,
+                    Toast.LENGTH_SHORT
+                ).show()
             }
-        }) //Part 3
-
-
+        })
 
 
     }
--- app/src/main/java/com/qa/myapplication/MainActivity2.kt
+++ app/src/main/java/com/qa/myapplication/MainActivity2.kt
@@ -4,10 +4,8 @@ import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.View
-import android.widget.Button
-import android.widget.Toast
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
-import com.google.gson.Gson
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -20,71 +18,55 @@ class MainActivity2 : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
 
-        val button =findViewById<Button>(R.id.goBack)
+        val button = findViewById<Button>(R.id.goBack)
         button.setOnClickListener {
-            val intent = Intent(this@MainActivity2,MainActivity::class.java)
+            val intent = Intent(this@MainActivity2, MainActivity::class.java)
             startActivity(intent)
-
-//        val sendD = findViewById<Button>(R.id.sendData)
-//            sendD.setOnClickListener {
-//                rawJ
-//            }
-
-            }
-
-
+        }
     }
 
-    fun rawJson(view: View){
+    fun rawJson(view: View) {
 
-        Log.i("Hello", "THIS IS MEEE")
-                val retrofit = Retrofit.Builder()
-                    .baseUrl("https://jsonplaceholder.typicode.com/")
-                    .addConverterFactory(GsonConverterFactory.create())
-                    .build()
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://jsonplaceholder.typicode.com/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
 
-                val api= retrofit.create(todoApi::class.java)
+        val api = retrofit.create(todoApi::class.java)
 
-                var todo = Todos(1,0,"Hey Hey",true)
+        val userId = findViewById<EditText>(R.id.userId).text.toString()
+        val title = findViewById<EditText>(R.id.title).text.toString()
+        val completed = findViewById<Spinner>(R.id.tORf).selectedItem.toString()
 
+        Log.d("This is the string to send?", userId + title + completed)
+
+        val todo = Todos(Integer.parseInt(userId), 0, title, completed.toBoolean())
 
         // Prepare the HTTP request
-        api.addTodos(todo).enqueue(object:Callback<Todos?>{
+        api.addTodos(todo).enqueue(object : Callback<Todos?> {
             override fun onResponse(call: Call<Todos?>, response: Response<Todos?>) {
-                val todo = response.body()
+                response.body()
 
-                if (response.code() == 201){
-                    Toast.makeText(applicationContext,"data sent"+response.isSuccessful,
-                        Toast.LENGTH_LONG).show()
-                }else{
-                    Toast.makeText(applicationContext,"SHITTT"+response.isSuccessful,
-                        Toast.LENGTH_LONG).show()
+                if (response.code() == 201) {
+                    Toast.makeText(
+                        applicationContext, "data sent" + response.isSuccessful,
+                        Toast.LENGTH_LONG
+                    ).show()
+                } else {
+                    Toast.makeText(
+                        applicationContext, "SHITTT" + response.isSuccessful,
+                        Toast.LENGTH_LONG
+                    ).show()
                 }
-
-
             }
 
             override fun onFailure(call: Call<Todos?>, t: Throwable) {
-                Toast.makeText(applicationContext, t.message,
-                    Toast.LENGTH_SHORT).show()
+                Toast.makeText(
+                    applicationContext, t.message,
+                    Toast.LENGTH_SHORT
+                ).show()
             }
         })
-
-                Log.i("runnnn", "done")
-
-//                var jsonObject = JSONObject()
-//                jsonObject.put("userId", 1)
-//                jsonObject.put("title", "Testing")
-//                jsonObject.put("completed", true)
-//
-//                var jsonObjectString = jsonObject.toString()
-//
-//
-//                api.addTodos(todo).enqueue(object : Callback<Todos>){
-//                    override fun onResponse(call:)
-//                }
-
-
-
+        Log.i("runnnn", "done")
     }
 }
